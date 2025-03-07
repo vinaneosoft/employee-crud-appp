54d7cb38a4f5962bf279ff04285f71994a3be709
vinaneosoft <vinaneosoft@gmail.com>
2025-03-07 14:02:44 +0530

    commit

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,9 +1,11 @@
 import './Header.css';
+import { Navbar } from '../navbar/Navbar';
 /* functional component */
 export function Header(){
     return(
         <header className="topheader">
             <h2>EMPLOYEE CRUD OPERATIONS</h2>
+            <Navbar></Navbar>
         </header>
     );
 }
