c7c80e77df19582891a92ffb96ec14641df6f161
vinaneosoft <vinaneosoft@gmail.com>
2025-03-11 10:50:11 +0530

    commit

--- src/employeecard/EmployeeCard.js
+++ src/employeecard/EmployeeCard.js
@@ -3,10 +3,10 @@
 export function EmployeeCard(){
     return (
         <div className="card" style={{width: "18rem"}}>
-        <img src={"https://st2.depositphotos.com/1594308/8523/i/450/depositphotos_85237228-stock-photo-happy-businessman-with-touchpad.jpg"} className="card-img-top"  />
-        <div className="card-body">
-            <h5 className="card-title"><span>#id</span> <span>Employee Name</span></h5>
-        </div>
+            <img src={"https://st2.depositphotos.com/1594308/8523/i/450/depositphotos_85237228-stock-photo-happy-businessman-with-touchpad.jpg"} className="card-img-top"  />
+            <div className="card-body">
+                <h5 className="card-title"><span>#id</span> <span>Employee Name</span></h5>
+            </div>
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">Department Code</li>
                 <li class="list-group-item">Joined on</li>
@@ -16,7 +16,6 @@ export function EmployeeCard(){
                 <a href="#" class="card-link">EDIT</a>
                 <a href="#" class="card-link">DELETE</a>
             </div>
-        
         </div>
     );
 }
