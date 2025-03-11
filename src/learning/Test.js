5be73d32700790ed97eaa016cabbbd1ccaaefbb8
vinaneosoft <vinaneosoft@gmail.com>
2025-03-11 14:08:53 +0530

    class component

---
+++ src/learning/Test.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Test extends Component{
+    render(){
+       
+        return  <React.Fragment>
+            <h1>This is class component</h1>
+            <h2>not allowed</h2>
+        </React.Fragment>
+    }
+}
+/*<React.Fragment>  </React.Fragment> can be written as <></>*/
