e938f22e022120de7cee5b92ae47274cfbddf012
vinaneosoft <vinaneosoft@gmail.com>
2025-03-12 12:21:25 +0530

    state manage

---
+++ src/businesslogic/companyaddress.js
@@ -0,0 +1,5 @@
+const  rabaleAddress="xyz, Rabale";
+
+const parelAddress="abc, Parel";
+
+export const dadarAddress="pqr, Dadar";
--- src/learning/Events.js
+++ src/learning/Events.js
@@ -4,19 +4,37 @@ Hook : useState */
 
 import { useState } from "react";
 
+import { dadarAddress } from "../businesslogic/companyaddress";
 
 export function Events(){
     /* [variable holding a state, function reference to set new state]*/
     let [companyName, setName]=useState("Neosoft Technologies");
+    let [companyAddress, setAddress]=useState(dadarAddress);
     function change(newname){
         console.log("in change funciton");
        // companyName=newname; // new state it wont work
         setName(newname); // this will work
     }
 
+    let [headingStyle, setBackColor]=useState({
+        background:'yellow',
+        color:'green',
+        textTransform:'uppercase'
+    })
+    function changeBackground(newbackcolor){
+        /* react rule : every object is immutable */
+       // headingStyle.background=newbackcolor // error
+        setBackColor({...headingStyle,background:newbackcolor});
+    }
+    
+
     return(<div>
-        <h4>{companyName}</h4>
+        <h4 onMouseOver={()=>changeBackground("pink")} style={headingStyle}>{companyName}</h4>
         <button onClick={()=>change("Neosoft")}>CHANGE COMPANY NAME</button>
+        <p>
+            {companyAddress}
+        </p>
+        <button>CHANGE ADDRESS</button>
     </div>);
 }
 
