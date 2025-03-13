fb1b0b6f0770d0200326cc34e80e888ee7ed5b31
vinaneosoft <vinaneosoft@gmail.com>
2025-03-13 10:47:35 +0530

    commit

--- src/employeeform/EmployeForm.js
+++ src/employeeform/EmployeForm.js
@@ -1,14 +1,36 @@
 
 export function EmployeeForm(){
     return(
+        <>
+        <h1>Employee Form</h1>
         <form>
-            {/*   make a bootstrap form
-       
-            name : text, departmentcode: Dropdown, 
-            joiningdate : datetime-local,experience :number,
-            submit Button, reset button 
-            */ }
+            <div class="mb-3">
+                <label for="exampleInputName" class="form-label">Name</label>
+                <input type="text" class="form-control" id="exampleInputName" />
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputNumber1" class="form-label">Experience</label>
+                <input type="number" class="form-control" id="exampleInputNumber1" />
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputNumber1" class="form-label">Date Of Join</label>
+                <input type="datetime-local" class="form-control" id="exampleInputNumber1" />
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputNumber1" class="form-label">Department</label>
+                <select class="form-select" aria-label="Default select example">
+                    <option selected>Python</option>
+                    <option value="JS">JavaScript</option>
+                    <option value="AD">Android</option>
+                    <option value="IOS">IOS</option>
+                    <option value="PHP">PHP</option>
+                    <option value="RJS">ReactJS</option>
+                </select>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="reset" class="btn btn-primary">Reset</button>
         </form>
+        </>
       
     );
 }
