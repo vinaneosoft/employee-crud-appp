ac092591335b16fd2259abf4901d8c16401b8235
vinaneosoft <vinaneosoft@gmail.com>
2025-03-13 11:26:56 +0530

    commit

--- src/employeeform/EmployeForm.js
+++ src/employeeform/EmployeForm.js
@@ -13,30 +13,33 @@ export function EmployeeForm(){
 
     function getDetails(ev){
         console.log(ev.target.value);
+        console.log(ev.target.id);
         //setName(ev.target.value);
     }
-
-
 
     return(
         <div className="border border-5 border-info bg-dark text-white" style={{margin:'5px 100px'}}>
         <h4 className="text-center">Employee Form</h4>
         <form className="m-5">
             <div className="mb-3">
-                <label htmlFor="exampleInputName" className="form-label">Name</label>
-                <input type="text" className="form-control" value={employee.employeeName} onChange={getDetails} id="exampleInputName" />
+                <label htmlFor="employeeName" className="form-label">Name</label>
+                <input type="text" className="form-control" value={employee.employeeName} 
+                onChange={getDetails} id="employeeName" />
             </div>
             <div className="mb-3">
-                <label htmlFor="exampleInputNumber1" className="form-label">Experience</label>
-                <input type="number" className="form-control" id="exampleInputNumber1" value={employee.experience} onChange={getDetails}  />
+                <label htmlFor="experience" className="form-label">Experience</label>
+                <input type="number" className="form-control" id="experience" value={employee.experience} 
+                onChange={getDetails}  />
             </div>
             <div className="mb-3">
-                <label htmlFor="exampleInputNumber1" className="form-label">Date Of Join</label>
-                <input type="datetime-local" className="form-control" id="exampleInputNumber1" value={employee.joiningDate} onChange={getDetails}  />
+                <label htmlFor="joiningDate" className="form-label">Date Of Joining</label>
+                <input type="datetime-local" className="form-control" id="joiningDate" 
+                value={employee.joiningDate} onChange={getDetails}  />
             </div>
             <div className="mb-3">
-                <label htmlFor="exampleInputNumber1" className="form-label">Department</label>
-                <select className="form-select" aria-label="Default select example" value={employee.departmentCode} onChange={getDetails} >
+                <label htmlFor="departmentCode" className="form-label">Department</label>
+                <select className="form-select" id="departmentCode" value={employee.departmentCode} 
+                onChange={getDetails} >
                     <option value="PT">Python</option>
                     <option value="JS">JavaScript</option>
                     <option value="AD">Android</option>
