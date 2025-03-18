9c88c382b2aa4e6b4427fa954386bbdfaecc365d
vinaneosoft <vinaneosoft@gmail.com>
2025-03-18 11:47:49 +0530

    commt

--- src/index.js
+++ src/index.js
@@ -15,10 +15,10 @@ let routes=[
     path:'/',
     element:  <App />,
     children :[
-       /*  {
+        {
             path:'/',
             loader:()=>redirect('adminlogin')
-        },  */
+        },  
         // when root path matches, one child must be automatically loaded
         {
             path:'adminlogin',
